1adc0f1e1c82b700ac450d3e254d3b98189419bf
HeXi037 <emilglad037@gmail.com>
2025-09-13 16:50:52 +1000

    Reconcile refresh_token table schemas

--- backend/alembic/versions/0013_merge_heads.py
+++ backend/alembic/versions/0013_merge_heads.py
@@ -1,7 +1,7 @@
 """merge heads
 
 Revision ID: 0013_merge_heads
-Revises: 0008_player_metric, 0012_refresh_token_table, 0012_refresh_tokens
+Revises: 0008_player_metric, 0013_reconcile_refresh_tokens
 Create Date: 2025-09-13 06:11:10.278954
 """
 
@@ -10,11 +10,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "0013_merge_heads"
-down_revision = (
-    "0008_player_metric",
-    "0012_refresh_token_table",
-    "0012_refresh_tokens",
-)
+down_revision = ("0008_player_metric", "0013_reconcile_refresh_tokens")
 branch_labels = None
 depends_on = None
 

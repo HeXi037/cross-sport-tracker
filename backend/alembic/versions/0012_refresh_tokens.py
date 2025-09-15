72989246ef16a081028a10598f862f454f2309d3
HeXi037 <emilglad037@gmail.com>
2025-09-15 20:30:30 +1000

    fix missing alembic revision

--- backend/alembic/versions/0012_refresh_tokens.py
+++ backend/alembic/versions/0012_refresh_tokens.py
@@ -2,7 +2,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision = '0012_refresh_tokens'
-down_revision = '0011_rehash_sha256_passwords'
+down_revision = '0007_rehash_sha256_passwords'
 branch_labels = None
 depends_on = None
 
